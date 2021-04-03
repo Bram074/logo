8fbb77dcd52c9d98f19c31b0fc444f6a5946c205
Bram074 <77251963+Bram074@users.noreply.github.com>
2021-04-03 21:53:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         $(".cls-3").css({"fill": newcolor})
         $(".cls-4").css({"fill": newcolor})
         $(".vcls-3").css({"fill": newcolor})
+        $(".label1").html(newcolor)
     });
 
     $("#color2").on("change", function(){
@@ -18,6 +19,7 @@ $(document).ready(function(){
         $(".cls-5").css({"fill": newcolor})
         $(".vcls-1").css({"fill": newcolor})
         $(".vcls-4").css({"fill": newcolor})
+        $(".label2").html(newcolor)
     });
 
 });
