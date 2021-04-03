de1f185d3d6c649da2a2b91d104243062550dd8e
Bram074 <77251963+Bram074@users.noreply.github.com>
2021-04-03 21:44:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+    $("#color1").on("change", function(){
+
+        var newcolor = $(this).val();
+
+        $(".cls-3").css({"fill": newcolor})
+        $(".cls-4").css({"fill": newcolor})
+        $(".vcls-3").css({"fill": newcolor})
+    });
+
+    $("#color2").on("change", function(){
+
+        var newcolor = $(this).val();
+
+        $(".cls-1").css({"fill": newcolor})
+        $(".cls-5").css({"fill": newcolor})
+        $(".vcls-1").css({"fill": newcolor})
+        $(".vcls-4").css({"fill": newcolor})
+    });
+
+});
